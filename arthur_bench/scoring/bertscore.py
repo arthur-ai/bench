29769a1f18be3e61ebeb432dc563e9c7fb2c7450
Reese Hyde <reese@arthur.ai>
2023-07-11 17:15:10 -0400

    LLM-323: suppress model load warning in transformers module

--- arthur_bench/scoring/bertscore.py
+++ arthur_bench/scoring/bertscore.py
@@ -1,6 +1,7 @@
 import bert_score
 from typing import List, Optional
 from arthur_bench.scoring import ScoringMethod
+from arthur_bench.scoring.utils import suppress_warnings
 
 # default model was chosen mainly for speed
 # only mild improvements to bertscore were observed using larger models on our test data
@@ -24,7 +25,10 @@ class BERTScore(ScoringMethod):
                   input_text_batch: Optional[List[str]] = None, context_batch: Optional[List[str]] = None) -> List[float]:
 
         # get precision, recall, and F1 score from bert_score package
-        p, r, f = bert_score.score(candidate_batch, reference_batch, lang='en', model_type=DEFAULT_MODEL, verbose=False)
+        # TODO: double check that tokenizer doesn't truncate by default
+        with suppress_warnings("transformers"):
+            p, r, f = bert_score.score(candidate_batch, reference_batch,
+                                       lang='en', model_type=DEFAULT_MODEL, verbose=False)
 
         # return a BERTScore using our weighting of precision and recall (instead of F1 which weights them equally)
         return (PRECISION_WEIGHT * p + RECALL_WEIGHT * r).tolist()
---
+++ arthur_bench/scoring/utils.py
@@ -0,0 +1,17 @@
+import logging
+
+
+class suppress_warnings:
+    """
+    A context-manager class to temporarily set the logging level for a logger to ERROR before returning it
+    to its previous state.
+    """
+    def __init__(self, logger_name: str):
+        self.logger = logging.getLogger(logger_name)
+        self.original_state = self.logger.level  # note: we get actual level not effective level, could be UNSET
+
+    def __enter__(self):
+        self.logger.setLevel(logging.ERROR)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.logger.setLevel(self.original_state)  # it's ok to use setLevel() to set to anything, even NOTSET
