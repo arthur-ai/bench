f6241e346e51fe75daa9675df3b4aa5f45210683
Max Cembalest <115359769+max-arthurai@users.noreply.github.com>
2023-07-14 10:12:24 -0400

    Fixed missing comma typo in context window map

--- arthur_bench/scoring/summary_quality.py
+++ arthur_bench/scoring/summary_quality.py
@@ -71,7 +71,7 @@ COMPARE = ChatPromptTemplate.from_messages(
 CONTEXT_WINDOW_MAP = {
 	'gpt-3.5-turbo' : 4096,
 	'gpt-4' : 8192,
-	'gpt-3.5-turbo-16k' : 16,384
+	'gpt-3.5-turbo-16k' : 16,384,
 	'gpt-4-32k' : 32,768
 }
 EVALUATOR_MODEL = 'gpt-3.5-turbo'
