7d404a62d2c74bc3f76056955b8cbf8e7deb6951
andrew-arthurai <andrew.witten@arthur.ai>
2023-07-21 10:45:55 -0400

    add logging

--- arthur_bench/telemetry/telemetry.py
+++ arthur_bench/telemetry/telemetry.py
@@ -1,25 +1,41 @@
-from amplitude import Amplitude, Identify, BaseEvent
+from amplitude import Amplitude, BaseEvent
 import logging
 import uuid
 import os
+from enum import Enum
+
 
 AMPLITUDE_API_KEY = 'bcc009e35e0b79daf089211108eed1de'
 amplitude = Amplitude(AMPLITUDE_API_KEY)
 
-TRACK_USAGE_DATA: bool = False
+class Telemetry(Enum):
+    ON = 1
+    OFF = 2
+    LOG = 3
+
+TRACK_USAGE_DATA: Telemetry = Telemetry.ON
 
 def set_track_usage_data():
     global TRACK_USAGE_DATA
-    # If env var BENCH_TELEMTRY_DISABLED is set to anything that is not 0, disable telemetry.
-    TRACK_USAGE_DATA = os.getenv('BENCH_TELEMTRY_DISABLED', "0") == "0"
 
-    if TRACK_USAGE_DATA:
-        logging.info("Telemetry data is being collected by Arthur! To disable, set the environment variable BENCH_TELEMTRY_DISABLED=1.")
+    telemetry = os.getenv('BENCH_TELEMTRY_DISABLED', "0")
+    if telemetry == "log":
+        TRACK_USAGE_DATA = Telemetry.LOG
+    elif telemetry != "0":
+        TRACK_USAGE_DATA = Telemetry.OFF
+
+    if TRACK_USAGE_DATA == Telemetry.ON:
+        logging.warn("""Telemetry data is being collected by Arthur! To disable, set the environment variable BENCH_TELEMTRY_DISABLED=1.
+                     To disable pushing metrics and instead log what would be pushed, set BENCH_TELEMTRY_DISABLED=log.""")
 
 
 # A no-op if user opts out of data collection.
-def send_event(event, user_id):
-    if not TRACK_USAGE_DATA:
+def send_event(event, user_id: uuid.UUID):
+    if TRACK_USAGE_DATA == Telemetry.OFF:
+        return
+
+    elif TRACK_USAGE_DATA == Telemetry.LOG:
+        logging.info(BaseEvent(user_id=str(user_id),**event))
         return
 
     logging.info("Pushing usage data to Arthur.")
