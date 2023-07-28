5d6e9823af7cbbf32d0546a4ef2afb9d9bc4faf4
andrew-arthurai <andrew.witten@arthur.ai>
2023-07-28 14:21:46 -0400

    update metrics

--- arthur_bench/server/run_server.py
+++ arthur_bench/server/run_server.py
@@ -45,7 +45,7 @@ def test_suites(request: Request):
         suites = duckdb.sql(f"SELECT name, description, created_at, scoring_method FROM read_json_auto('{SERVER_ROOT_DIR}/*/suite.json', timestampformat='{TIMESTAMP_FORMAT}')").df().to_dict('records')
     except duckdb.IOException:
         suites = []
-    send_event({"event_type": "test_suites_real", "event_properties": {"num_test_suites_real": len(suites), "test_suites_all": [{"created_at": suite['created_at'], "scoring_method": suite['scoring_method']} for suite in suites]}}, USER_ID)
+    send_event({"event_type": "test_suites_load", "event_properties": {"num_test_suites_load": len(suites), "test_suites_all": [suite['scoring_method'] for suite in suites]}}, USER_ID)
     return templates.TemplateResponse("test_suite_overview.html", {"request": request,
                                                                    "suites": suites})
 
@@ -58,7 +58,7 @@ def test_runs(request: Request, test_suite_name: str):
     except duckdb.IOException:
         runs = []
         suite = "Unknown"
-    send_event({"event_type": "test_runs_real", "event_properties": {"test_runs_all": [{"created_at": run['created_at']} for run in runs], "scoring_method_real": suite}}, USER_ID)
+    send_event({"event_type": "test_runs_load", "event_properties": {"test_runs_all": [run['created_at'] for run in runs], "scoring_method_real": suite}}, USER_ID)
     return templates.TemplateResponse("test_run_overview.html", {"request": request,
                                                                  "runs": runs,
                                                                  "test_suite_name": test_suite_name})
