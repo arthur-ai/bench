068100f8ffa59f263e32ceddf56b0d961f092272
Rowan Cheung <rowan@arthur.ai>
2023-08-09 10:07:00 -0400

    use correct dir

--- arthur_bench/client/local/client.py
+++ arthur_bench/client/local/client.py
@@ -139,8 +139,8 @@ class LocalBenchClient(BenchClient):
             root_dir = _bench_root_dir()
 
         # if root dir does not exist, create:
-        if not os.path.exists(_bench_root_dir()):
-            os.mkdir(_bench_root_dir())
+        if not os.path.exists(root_dir)):
+            os.mkdir(root_dir)
         
         self.root_dir = Path(root_dir)
         _write_suite_index(self.root_dir)
