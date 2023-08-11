6fa1de103f3b30779d5bed74a6b4836b069cf86c
max.cembalest <max.cembalest@arthur.ai>
2023-08-11 16:26:19 -0400

    fixed hallucination score unit test

--- test/test_hallucination.py
+++ test/test_hallucination.py
@@ -1,30 +1,34 @@
 import pytest
+import os
 from unittest.mock import Mock, patch
 
+from helpers import get_mock_client
 from arthur_bench.scoring.hallucination import Hallucination
 
 @pytest.fixture
-def mock_hallucination_request():
-    hallucination_request = Mock()
-    hallucination_request.return_value = {"hallucination": "0", "reason": "this is the reason"}
-    return hallucination_request
+def mock_client():
+    mock_client = get_mock_client()
+    mock_client.bench = Mock()
+    mock_client.bench.score_hallucination = Mock()
+    mock_client.bench.score_hallucination.return_value = {"hallucination": "0", "reason": "this is the reason"}
+    return mock_client
 
 # Test the run_batch method
-def test_run_batch(mock_hallucination_request, mock_summary_data):
-    with patch('Hallucination.client.bench.score_hallucination', return_value=mock_hallucination_request):
+def test_run_batch(mock_client, mock_summary_data):
+    with patch('arthur_bench.scoring.hallucination.ArthurClient', return_value=mock_client):
 
-        # create summary quality scoring method
+        # create summary quality scoring method (with a mocked client)
         hallucination_score = Hallucination()
 
-        # get run batch result
+        # get run batch result from mock client call 
         result = hallucination_score.run_batch(
             mock_summary_data['candidate_summary'],
-            mock_summary_data['source']
+            context_batch=mock_summary_data['source']
         )
 
-        # assert LLMChain called with correct parameters
+        # assert mock score_hallucination called with correct parameters
         for i in range(len(mock_summary_data)):
-            mock_hallucination_request.assert_any_call({
+            hallucination_score.client.bench.score_hallucination.assert_any_call({
                 "response": mock_summary_data['candidate_summary'][i],
                 "context": mock_summary_data['source'][i]})
 
