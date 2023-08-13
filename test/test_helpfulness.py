85871e1cedaf18fdc992dcc276c1905afcc65245
Rowan Cheung <rowan@arthur.ai>
2023-08-13 16:44:14 -0400

    increase code coverage and remove global variables from server

--- test/test_helpfulness.py
+++ test/test_helpfulness.py
@@ -1,37 +1,31 @@
-import pytest
-from unittest.mock import Mock, MagicMock, patch
 import torch
 
+from fixtures.mock_data import MOCK_SUMMARY_DATA
 from arthur_bench.scoring.readability import Readability
 from arthur_bench.scoring.word_count_match import WordCountMatch
-from textstat import flesch_reading_ease
 
 
-def test_run_readability(mock_summary_data):
+def test_run_readability():
+    readability = Readability()
 
-        readability = Readability()
+    readability_run_result = readability.run_batch(
+        MOCK_SUMMARY_DATA["candidate_summary"], MOCK_SUMMARY_DATA["summary"]
+    )
 
-        readability_run_result = readability.run_batch(
-            mock_summary_data['candidate_summary'],
-            mock_summary_data['summary']
-        )
-
-        # assert return correct values
-        expected = [44.41, 91.27, 68.77, 64.37]
-        for i, result in enumerate(readability_run_result):
-            assert torch.isclose(torch.tensor(result), torch.tensor(expected[i]), atol=1e-5)
+    # assert return correct values
+    expected = [44.41, 91.27, 68.77, 64.37]
+    for i, result in enumerate(readability_run_result):
+        assert torch.isclose(torch.tensor(result), torch.tensor(expected[i]), atol=1e-5)
 
 
-def test_run_wcm(mock_summary_data):
+def test_run_wcm():
+    word_count_match = WordCountMatch()
 
-        word_count_match = WordCountMatch()
+    wcm_run_result = word_count_match.run_batch(
+        MOCK_SUMMARY_DATA["candidate_summary"], MOCK_SUMMARY_DATA["summary"]
+    )
 
-        wcm_run_result = word_count_match.run_batch(
-            mock_summary_data['candidate_summary'],
-            mock_summary_data['summary']
-        )
-
-        # assert return correct values
-        expected = [0.8888888, 0.7857142, 1.0, 0.583333]
-        for i, result in enumerate(wcm_run_result):
-            assert torch.isclose(torch.tensor(result), torch.tensor(expected[i]), atol=1e-5)
+    # assert return correct values
+    expected = [0.8888888, 0.7857142, 1.0, 0.583333]
+    for i, result in enumerate(wcm_run_result):
+        assert torch.isclose(torch.tensor(result), torch.tensor(expected[i]), atol=1e-5)
