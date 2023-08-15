e09d5265e18849b6b4ffc1f6f6e666493ba164c7
teresadatta100 <121882544+teresadatta100@users.noreply.github.com>
2023-08-15 14:08:15 -0400

    fixing test fixtures

--- tests/scoring_methods/test_helpfulness.py
+++ tests/scoring_methods/test_helpfulness.py
@@ -2,6 +2,8 @@ import pytest
 from unittest.mock import Mock, MagicMock, patch
 import torch
 
+
+from tests.fixtures.mock_data import MOCK_SUMMARY_DATA
 from arthur_bench.scoring.readability import Readability
 from arthur_bench.scoring.word_count_match import WordCountMatch
 from arthur_bench.scoring.specificity import Specificity
@@ -32,13 +34,13 @@ def mock_lexicon_count():
     return scorer
 
 
-def test_run_readability(mock_summary_data):
+def test_run_readability():
 
         readability = Readability()
 
         readability_run_result = readability.run_batch(
-            mock_summary_data['candidate_summary'],
-            mock_summary_data['summary']
+            MOCK_SUMMARY_DATA['candidate_summary'],
+            MOCK_SUMMARY_DATA['summary']
         )
 
         # assert return correct values
@@ -47,13 +49,13 @@ def test_run_readability(mock_summary_data):
             assert torch.isclose(torch.tensor(result), torch.tensor(expected[i]), atol=1e-5)
 
 
-def test_run_wcm(mock_summary_data):
+def test_run_wcm():
 
         word_count_match = WordCountMatch()
 
         wcm_run_result = word_count_match.run_batch(
-            mock_summary_data['candidate_summary'],
-            mock_summary_data['summary']
+            MOCK_SUMMARY_DATA['candidate_summary'],
+            MOCK_SUMMARY_DATA['summary']
         )
 
         # assert return correct values
@@ -61,7 +63,7 @@ def test_run_wcm(mock_summary_data):
         for i, result in enumerate(wcm_run_result):
             assert torch.isclose(torch.tensor(result), torch.tensor(expected[i]), atol=1e-5)
 
-def test_specificity(mock_summary_data):
+def test_specificity(mock_get_mean_word_freq, mock_get_num_vague_words, mock_get_pn_and_num, mock_lexicon_count):
     with (
         patch('arthur_bench.scoring.specificity.Specificity.get_num_vague_words') as mock_get_num_vague_words,
         patch('arthur_bench.scoring.specificity.Specificity.get_mean_word_freq') as mock_get_mean_word_freq,
@@ -72,15 +74,15 @@ def test_specificity(mock_summary_data):
         specificity = Specificity()
 
         spec_run_result = specificity.run_batch(
-            mock_summary_data['candidate_summary'],
-            mock_summary_data['summary']
+            MOCK_SUMMARY_DATA['candidate_summary'],
+            MOCK_SUMMARY_DATA['summary']
         )
 
         #assert mock functions called 
-        mock_get_num_vague_words.assert_called_with(mock_summary_data['candidate_summary'][-1])
-        mock_get_mean_word_freq.assert_called_with(mock_summary_data['candidate_summary'][-1])
-        mock_get_pn_and_num.assert_called_with(mock_summary_data['candidate_summary'][-1])
-        mock_lexicon_count.assert_called_with(mock_summary_data['candidate_summary'][-1])
+        mock_get_num_vague_words.assert_called_with(MOCK_SUMMARY_DATA['candidate_summary'][-1])
+        mock_get_mean_word_freq.assert_called_with(MOCK_SUMMARY_DATA['candidate_summary'][-1])
+        mock_get_pn_and_num.assert_called_with(MOCK_SUMMARY_DATA['candidate_summary'][-1])
+        mock_lexicon_count.assert_called_with(MOCK_SUMMARY_DATA['candidate_summary'][-1])
 
         #assert return correct values
         expected = [0.8250, 0.8250, 0.8250, 0.8250]
