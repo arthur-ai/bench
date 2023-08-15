3d807ed8e970a10b21a78e9a295ad544da760fd0
cheungrowan <138825305+cheungrowan@users.noreply.github.com>
2023-08-15 17:40:04 -0400

    Getting started ux (#35)
    * update examples directory
    
    * return suite num runs and update client tests
    
    * add multiple scoring method filter support
    
    * remove old file system relics while i'm here
    
    * Update unit-test.yml
    
    * added coding solutions to examples
    
    * fixed dir typo
    
    * add python unit testing to examples dir
    
    ---------
    
    Co-authored-by: max.cembalest <max.cembalest@arthur.ai>

--- arthur_bench/client/bench_client.py
+++ arthur_bench/client/bench_client.py
@@ -1,4 +1,4 @@
-from typing import Optional, TypeVar
+from typing import Optional, TypeVar, List
 from abc import ABC, abstractmethod
 
 from arthur_bench.models.models import (
@@ -16,46 +16,43 @@ TBenchClient = TypeVar("TBenchClient", bound="BenchClient")
 
 
 class BenchClient(ABC):
-
     @abstractmethod
     def get_test_suites(
         self,
         name: Optional[str] = None,
         sort: Optional[str] = None,
-        scoring_method: Optional[str] = None,
+        scoring_method: Optional[List[str]] = None,
         page: int = 1,
-        page_size: int = 5
+        page_size: int = 5,
     ) -> PaginatedTestSuites:
         raise NotImplementedError
-    
+
     @abstractmethod
     def create_test_suite(self, json_body: TestSuiteRequest) -> PaginatedTestSuite:
         raise NotImplementedError
-    
+
     @abstractmethod
     def get_test_suite(
-        self, 
-        test_suite_id: str,
-        page: int = 1,
-        page_size: int = 5) -> PaginatedTestSuite:
+        self, test_suite_id: str, page: int = 1, page_size: int = 5
+    ) -> PaginatedTestSuite:
         raise NotImplementedError
-    
+
     @abstractmethod
     def get_runs_for_test_suite(
-        self, 
-        test_suite_id: str, 
+        self,
+        test_suite_id: str,
         sort: Optional[str] = None,
         page: int = 1,
-        page_size: int = 5
+        page_size: int = 5,
     ) -> PaginatedRuns:
         raise NotImplementedError
-    
+
     @abstractmethod
     def create_new_test_run(
         self, test_suite_id: str, json_body: CreateRunRequest
     ) -> CreateRunResponse:
         raise NotImplementedError
-    
+
     @abstractmethod
     def get_test_run(
         self,
@@ -66,21 +63,21 @@ class BenchClient(ABC):
         sort: Optional[bool] = None,
     ) -> PaginatedRun:
         raise NotImplementedError
-    
+
     @abstractmethod
     def get_summary_statistics(
         self,
         test_suite_id: str,
         run_id: Optional[str] = None,
         page: int = 1,
-        page_size: int = 5
+        page_size: int = 5,
     ) -> TestSuiteSummary:
         raise NotImplementedError
-    
+
     @abstractmethod
     def delete_test_suite(self, test_suite_id: str):
         raise NotImplementedError
-    
+
     @abstractmethod
     def delete_test_run(self, test_suite_id: str, test_run_id: str):
         raise NotImplementedError
